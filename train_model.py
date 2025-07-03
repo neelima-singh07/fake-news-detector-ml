cc19f6bed7c4a83f8358cd7c56f9c3c471dd5e19
neelima_singh <neelima.arun2007@gmail.com>
2025-07-03 19:42:05 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,36 +6,36 @@ from sklearn.metrics import accuracy_score
 import pickle
 import os
 
-# Load datasets
+
 true_df = pd.read_csv("data/True.csv")
 fake_df = pd.read_csv("data/Fake.csv")
 
-# Label the data
+
 true_df["label"] = 1  # Real
 fake_df["label"] = 0  # Fake
 
-# Combine
+
 df = pd.concat([true_df, fake_df])
 df["content"] = df["title"] + " " + df["text"]
 df = df[["content", "label"]]
 
-# Split data
+
 X_train, X_test, y_train, y_test = train_test_split(df["content"], df["label"], test_size=0.2, random_state=42)
 
-# TF-IDF vectorizer
+
 vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7)
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
-# Logistic Regression
+# aplly Logistic Regression
 model = LogisticRegression()
 model.fit(X_train_vec, y_train)
 
-# Evaluate
+# accuracy 
 y_pred = model.predict(X_test_vec)
 print("✅ Accuracy:", accuracy_score(y_test, y_pred))
 
-# Save model & vectorizer
+
 os.makedirs("model", exist_ok=True)
 pickle.dump(model, open("model/model.pkl", "wb"))
 pickle.dump(vectorizer, open("model/vectorizer.pkl", "wb"))
